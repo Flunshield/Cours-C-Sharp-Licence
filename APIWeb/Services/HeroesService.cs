9caaf66415c80cf2e676dc5910ff86da1872722f
BERTRAND <jbertrand@ecole-isitech.fr>
2022-12-14 18:58:29 +0100

    Feat: Generating a random player

--- APIWeb/Services/HeroesService.cs
+++ APIWeb/Services/HeroesService.cs
@@ -14,41 +14,23 @@ namespace APIWeb.Services
         {
             var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
             var random = new Random();
-            var nameSize = new char[random.Next(8)];
-            var classePlayerSize = new int[random.Next(3)];
-
-            string[] classePlayer =
-            {
-                "Warrior", "Mage", "Thief"
-            };
+            var nameSize = new char[random.Next(3,9)];
+            string[] classePlayer = { "Warrior", "Mage", "Thief" };
+            int classePlayerGenerateNumber = random.Next(classePlayer.Length);
 
             for (int i = 0; i < nameSize.Length; i++)
             {
                 nameSize[i] = characters[random.Next(characters.Length)];
             }
 
-            for (int i = 0; i < classePlayerSize.Length; i++)
-            {
-                classePlayerSize[i] = characters[random.Next(3)];
-            }
-
             string namePlayerGenerate = new string (nameSize);
-            string classePlayerGenerate = classePlayer[classePlayerSize];
-            var forcePlayer = new int[random.Next(101)];
-            var sagessePlayer = new int[random.Next(101)];
-            var vitalityPlayer = new int[random.Next(101)];
+            
+            int forcePlayer = random.Next(1,101);
+            int sagessePlayer = random.Next(101);
+            int vitalityPlayer = random.Next(101);
+            string classePlayerGenerate = classePlayer[classePlayerGenerateNumber];
 
-            Console.WriteLine(namePlayerGenerate + " " + classePlayerGenerate + " " + forcePlayer + " " + sagessePlayer + " " + vitalityPlayer);
 
         }
     }
 }
-
-    /**
-     *     public string name { get; set; } = string.Empty;
-
-    public long force { get; set; }
-
-    public long sagesse { get; set; }
-
-    public long vitality { get; set; }*/
